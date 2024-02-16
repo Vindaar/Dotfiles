9be236e5b376867ca35b581ea202cc0ca291f1e7
Vindaar <basti90@gmail.com>
2024-02-16 16:34:08 +0100

    [zsh] update autocompletion logic for cligen programs

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,13 +3,24 @@
 autoload -U compinit && compinit
 zmodload -i zsh/complist
 
+# define a custom function for cligen autocompletion
+# load the custom completer for cligen commands, which disables color
+_cligen() {
+    # For cligen commands (which follow the GNU -h/--help style), disable color output in the context
+    # of `_gnu_generic`:
+    NO_COLOR=1 _gnu_generic
+}
+
 # Add path to my completions
 fpath=(~/.zsh/completion.local $fpath)
+
+# add custom completers
+fpath=(~/.zsh/completions $fpath)
 
 # man zshcontrib
 zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
 zstyle ':vcs_info:*' formats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
-zstyle ':vcs_info:*' enable git #svn cvs 
+zstyle ':vcs_info:*' enable git #svn cvs
 
 # Enable completion caching, use rehash to clear
 zstyle ':completion::complete:*' use-cache on
@@ -28,7 +39,8 @@ zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
 # list of completers to use
-zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
+zstyle ':completion:*::::' completer _expand _complete _cligen _ignored _approximate
+
 
 zstyle ':completion:*' menu select=1 _complete _ignored _approximate
 
@@ -39,11 +51,14 @@ zstyle ':completion:*' menu select=1 _complete _ignored _approximate
 # 1 -- smart case completion (abc => Abc)
 # 2 -- word flex completion (abc => A-big-Car)
 # 3 -- full flex completion (abc => ABraCadabra)
-zstyle ':completion:*' matcher-list '' \
-  'm:{a-z\-}={A-Z\_}' \
-  'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
-  'r:|?=** m:{a-z\-}={A-Z\_}'
- 
+
+## NOTE: This setting *BREAKS* the `cligen` completion logic!!
+
+#zstyle ':completion:*' matcher-list '' \
+#  'm:{a-z\-}={A-Z\_}' \
+#  'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
+#  'r:|?=** m:{a-z\-}={A-Z\_}'
+#
 # offer indexes before parameters in subscripts
 zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
 
@@ -54,7 +69,7 @@ zstyle ':completion:*:messages' format '%d'
 zstyle ':completion:*:warnings' format 'No matches for: %d'
 zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
 zstyle ':completion:*' group-name ''
- 
+
 # ignore completion functions (until the _ignored completer)
 zstyle ':completion:*:functions' ignored-patterns '_*'
 zstyle ':completion:*:scp:*' tag-order files users 'hosts:-host hosts:-domain:domain hosts:-ipaddr"IP\ Address *'
